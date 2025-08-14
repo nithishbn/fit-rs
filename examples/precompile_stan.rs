425dff214d353a37d8f503f6a80aa954c816b8be
Nithish Narasimman <nithish@octant.bio>
2025-08-13 17:40:49 -0700

    dont think this is necessary

--- examples/precompile_stan.rs
+++
@@ -1,8 +0,0 @@
-use fit_rs::precompile_stan_model;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Precompiling Stan model...");
-    precompile_stan_model()?;
-    println!("Stan model precompilation complete!");
-    Ok(())
-}
